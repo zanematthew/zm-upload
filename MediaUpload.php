94d3e90e9e72012df4c742675cdc7f9b376b41ea
Zane M. Kolnik <zanematthew@gmail.com>
2012-07-13 09:40:55 -0400

    added new method to help with resize, saveupload now returns an array of goodness vs. just an id

--- MediaUpload.php
+++ MediaUpload.php
@@ -11,7 +11,13 @@
 require_once 'ImageMeta.php';
 Class MediaUpload {
 
+    public $upload_dir;
+    private $attachment_id;
+
     public function __construct(){
+
+        $this->upload_dir = wp_upload_dir();
+
         if ( is_admin() )
             add_action( 'post_edit_form_tag' , array( &$this, 'addEnctype' ) );
     }
@@ -22,7 +28,11 @@ Class MediaUpload {
      * During form submission run the method:
      * $class->fileUpload( $field_name='form_field_name' );
      *
-     * @return attachmentID on success false on failure
+     * @return $final_file An array of array of f*cking cool stuff
+     * I guess if you think arrays are cool i like (*)(*)s
+     * $final_file['attachment_id'] = $this->attachment_id;
+     * $final_file['file'] = $uploaded_file['file'];
+     * $final_file['file_info'] = $file_info[];
      */
     public function saveUpload( $field_name=null, $user_id=null ) {
 
@@ -57,25 +67,35 @@ Class MediaUpload {
             'post_date_gmt' => date( 'Y-m-d H:i:s' )
         );
 
-
         // Add the file to the media library and generate thumbnail.
-        $attach_id = wp_insert_attachment( $attachment, $uploaded_file['file'] );
+        $this->attachment_id = wp_insert_attachment( $attachment, $uploaded_file['file'] );
 
+        // @todo bug, this does NOT work when used in a PLUGIN!, so you'll have to make
+        // your OWN thumbnail sizes!
         require_once( ABSPATH . "wp-admin" . '/includes/image.php' );
-        $meta = wp_generate_attachment_metadata( $attach_id, $uploaded_file['file'] );
+        $meta = wp_generate_attachment_metadata( $this->attachment_id, $uploaded_file['file'] );
+
+        $image_meta = wp_read_image_metadata( $uploaded_file['file'] );
+        $meta['image_meta'] = $image_meta;
 
         $image = New ImageMeta;
         $meta['image_meta']['keywords'] = $image->iptcParser( 'keywords', $uploaded_file['file'] );
         $meta['image_meta']['city']     = $image->iptcParser( 'city',     $uploaded_file['file'] );
         $meta['image_meta']['region']   = $image->iptcParser( 'region',   $uploaded_file['file'] );
         $meta['image_meta']['country']  = $image->iptcParser( 'country',  $uploaded_file['file'] );
+        wp_update_attachment_metadata( $this->attachment_id, $meta );
 
-        wp_update_attachment_metadata( $attach_id, $meta );
+        $file_info = pathinfo( $uploaded_file['file'] );
 
         // Set the feedback flag to false, since the upload was successful
         $upload_feedback = false;
 
-        return $attach_id;
+        $final_file = array();
+        $final_file['attachment_id'] = $this->attachment_id;
+        $final_file['file'] = $uploaded_file['file'];
+        $final_file['file_info'] = $file_info;
+
+        return $final_file;
     }
 
     /**
@@ -92,8 +112,8 @@ Class MediaUpload {
      * @return guid
      */
     public function buildGuid( $file=null ){
-        $wp_upload_dir = wp_upload_dir();
-        return $wp_upload_dir['baseurl'] . '/' . _wp_relative_upload_path( $file );
+        // $wp_upload_dir = wp_upload_dir();
+        return $this->upload_dir['baseurl'] . '/' . _wp_relative_upload_path( $file );
     }
 
     /**
@@ -110,5 +130,56 @@ Class MediaUpload {
      */
     public function addEnctype(){
         echo ' enctype="multipart/form-data"';
+    }
+
+    /**
+     * Resize images based on the "type"
+     *
+     * Normally this is done in WordPress, but for some reason
+     * wp_generate_attachment_metadata() does not work when
+     * used in a plugin.
+     *
+     * @param $file = /my/file/path/image.jpg
+     * @param $type = thumb|square|main
+     *
+     * @todo Since images are NOT "registered" with WordPress
+     * they will NOT be deleted from the media library when the
+     * original image is deleted!
+     *
+     * @todo use wp_update_attachment_metadata() to update
+     * the postmeta thumbnails ref. the array in
+     * wp_generate_attachment_metadata()
+     *
+     * @todo remove hardcoded sizes and suffix, possibly a
+     * public variable.
+     *
+     * @return same as image_resize() wp_error
+     */
+    public function resizeImage( $file=null, $type=null ){
+        switch ( $type ) {
+            case 'thumb':
+                $max_w = 104;
+                $max_h = 70;
+                $suffix = 'zm-thumb';
+                break;
+
+            case 'square':
+                $max_w = 50;
+                $max_h = 50;
+                $suffix = 'zm-square';
+                break;
+
+            case 'main':
+                $max_w = 454;
+                $max_h = 300;
+                $suffix = 'zm-main';
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
+        return image_resize( $file, $max_w, $max_h, $crop=true, $suffix, $path=$this->upload_dir['path'] );
     }
 } // End 'MediaUpload'
